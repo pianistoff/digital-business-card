a7cefc63588cb5dc104d5888c49a8b3e13c915ed
pianistoff <severyn.h@gmail.com>
2022-05-28 02:50:19 +0300

    Make js components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
+import Info from "./Info";
+import About from "./About";
+import Interests from "./Interests";
+import Footer from "./Footer";
+
 export default function App() {
     return (
         <>
-            
+            <Info />
+            <About />
+            <Interests />
+            <Footer />
         </>
     )
 }
