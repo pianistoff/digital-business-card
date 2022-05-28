d2deeb26f6a111ff2c1362f6aa9ede44a964afe5
pianistoff <severyn.h@gmail.com>
2022-05-28 10:42:26 +0300

    Stylize interest component

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,8 +1,8 @@
 export default function Interests() {
     return (
         <div className="interests">
-            <h2>Interests</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam, alias. Blanditiis magni aspernatur beatae est dolorem vitae aut, iste explicabo veritatis consequatur esse rerum, voluptate optio maiores natus dolore saepe?</p>
+            <h2 className="interests-title">Interests</h2>
+            <p className="interests-para">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam, alias. Blanditiis magni aspernatur beatae est dolorem vitae aut, iste explicabo veritatis consequatur esse rerum, voluptate optio maiores natus dolore saepe?</p>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import "./index.css"
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/700.css";
+import "./index.css";
 
 ReactDOM.render(
     <App />,
