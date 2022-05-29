2c8cef07d86c57610fb62c24ae76d2ad12cff5ba
pianistoff <severyn.h@gmail.com>
2022-05-29 20:48:32 +0300

    Turn social icons into links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,11 +7,11 @@ import github from "../img/github.svg";
 export default function Footer() {
     return (
         <footer className="footer">
-            <img src={twitter} alt="twitter icon" className="social-link" />
-            <img src={facebook} alt="facebook icon" className="social-link" />
-            <img src={instagram} alt="instagram icon" className="social-link" />
-            <img src={linkedin} alt="linkedin icon" className="social-link" />
-            <img src={github} alt="github icon" className="social-link" />
+            <a href="https://twitter.com/severyn_h"><img src={twitter} alt="twitter icon" className="social-link" /></a>
+            <a href="https://www.facebook.com/severyn.hundiak/"><img src={facebook} alt="facebook icon" className="social-link" /></a>
+            <a href="https://www.instagram.com/severino_il_pianista/"><img src={instagram} alt="instagram icon" className="social-link" /></a>
+            <a href="https://www.linkedin.com/in/severyn-hundiak-839b4421b/"><img src={linkedin} alt="linkedin icon" className="social-link" /></a>
+            <a href="http://github.com/pianistoff/"><img src={github} alt="github icon" className="social-link" /></a>
         </footer>
     );
 }
