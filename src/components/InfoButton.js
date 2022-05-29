39c11032f99afa75374c1ae68ae286b66ab50402
pianistoff <severyn.h@gmail.com>
2022-05-29 20:35:19 +0300

    Finish styling

--- src/components/InfoButton.js
+++ src/components/InfoButton.js
@@ -1,10 +1,10 @@
-import mail from "../img/mail.svg"
+import mail from "../img/mail.svg";
 
 export default function InfoButton() {
     return (
-        <a href="mailto:severyn.h@gmail.com">
-            <img src={mail} alt="mail icon" />
-            <span>Email</span>
+        <a href="mailto:severyn.h@gmail.com" className="email-button">
+            <img src={mail} alt="mail icon" className="mail-icon" />
+            <span className="button-text">Email</span>
         </a>
-    )
+    );
 }
