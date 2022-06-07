940f89f86505c573bc741b444f300f18bea8379a
pianistoff <severyn.h@gmail.com>
2022-06-07 10:33:35 +0300

    Fix top of the card not seen when viewport's height is small

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -2,7 +2,7 @@ export default function Interests() {
     return (
         <div className="interests">
             <h2 className="interests-title title">Interests</h2>
-            <p className="interests-para para">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam, alias. Blanditiis magni aspernatur beatae est dolorem vitae aut, iste explicabo veritatis consequatur esse rerum, voluptate optio maiores natus dolore saepe?</p>
+            <p className="interests-para para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore voluptate facere ducimus unde numquam sed expedita accusantium rem corporis ullam sit asperiores eligendi, harum cupiditate cumque repellat. Laborum, voluptatum reiciendis.</p>
         </div>
     );
 }
